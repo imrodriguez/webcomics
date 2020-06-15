ef12e258b29a00429aeb8b4e50c80f450c0de824
imanol <imanol.rodriguez@fhios.es>
2020-06-15 15:42:04 +0200

    Updates

---
+++ precache-manifest.f9853660c16773b3194ef7f24a7c9958.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b36d08b34a9aca5c138e0610a0f890",
+    "url": "/webcomics/index.html"
+  },
+  {
+    "revision": "184065fbdc51800f5369",
+    "url": "/webcomics/static/js/2.aa28ed91.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webcomics/static/js/2.aa28ed91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e294797d28af0c5f500",
+    "url": "/webcomics/static/js/main.6ccb6d83.chunk.js"
+  },
+  {
+    "revision": "0d7b0383a78e06421a9f",
+    "url": "/webcomics/static/js/runtime-main.84486966.js"
+  }
+]);
